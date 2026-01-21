085235e75817923949043e935f17d005906b2573
ANGEL GUPTA <022006angel@gmail.com>
2026-01-21 22:35:12 +0530

    nav

--- script.js
+++ script.js
@@ -73,11 +73,19 @@ window.addEventListener("scroll", () => {
   });
 });
 
-// ====== HAMBURGER ======
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
+const overlay = document.getElementById("overlay");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navLinks.classList.toggle("show");
+  overlay.classList.toggle("show");
 });
+
+overlay.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navLinks.classList.remove("show");
+  overlay.classList.remove("show");
+});
+
