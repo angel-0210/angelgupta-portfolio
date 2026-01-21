8223332779f04bed60d48e3292335fb1213b13ef
ANGEL GUPTA <022006angel@gmail.com>
2026-01-21 22:27:49 +0530

    nav bar update

--- script.js
+++ script.js
@@ -72,3 +72,11 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+//hamburger 
+const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("show");
+  });
